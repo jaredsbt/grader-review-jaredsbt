1e354a9381e17f9506c938e954af85144afc08da
Jared Rosas <61575769+jaredsbt@users.noreply.github.com>
2023-11-28 12:57:46 -0800

    Fixed grade bash script

--- grade.sh
+++ grade.sh
@@ -23,17 +23,37 @@ cd grading-area
 javac -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar *.java
 java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > output.txt
 
-if ! grep -q "Failures" output.txt; 
-    then
-        echo "100% test complete"
-    
-else
-    echo "You have some failures"
-fi
+
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
 
-# java TestListExamples1 > output.txt
+if ! grep -q "Failures" output.txt; 
+    then
+        echo "100% test complete"
+    
+else
+    echo "You have some failures"
+
+    line=`grep "Tests" output.txt`
+    testsCheck=""
+    numOfTests=0
+    numOfFails=0
+
+    # iterates words instead of characters, allows for loop to check characters past the colons (Ex. Failures: 3)
+    IFS=', ' read -ra words <<< "$line"
+
+    echo $line
+    for char in "${words[@]}"; do 
+        if [[ $char =~ ^[0-9]+$ ]] && [[ $testsCheck == "" ]]; then
+            numOfTests=$char
+            testsCheck+="Check"
+        elif [[ $char =~ ^[0-9]+$ ]]; then
+            numOfFails=$char
+        fi 
+    done
+    
+    echo "Tests passed: $((numOfTests - numOfFails)) / $numOfTests"
+fi
